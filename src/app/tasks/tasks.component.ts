e9852111897f1f6059de43bc18665154ada843cd
candace <candace.fogarty@hotmail.com>
2024-12-03 17:12:43 -0700

    created tasks.service file and moved appropriate methods over to this new class

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,8 +1,9 @@
 import { Component, Input } from '@angular/core';
-import { TaskComponent } from "./task/task.component";
-import { NewTaskComponent } from "./new-task/new-task.component";
 
 import { NewTaskData } from './task/task.model';
+import { TaskComponent } from "./task/task.component";
+import { NewTaskComponent } from "./new-task/new-task.component";
+import { TasksService } from './tasks.service';
 
 @Component({
   selector: 'app-tasks',
@@ -15,50 +16,19 @@ export class TasksComponent {
   @Input({ required: true }) userId! : string;
   @Input({ required: true }) name!: string;
   isAddingTask = false;
-  tasks = [
-    {
-      id: 't1',
-      userId: 'u1',
-      title: 'Master Angular',
-      summary: 'Learn all basic and advanced features of Angular &7 how to apply them.',
-      dueDate: '2025-12-31'
-    },
-    {
-      id: 't2',
-      userId: 'u3',
-      title: 'Build first prototype',
-      summary: 'Build a first prototype of the online shop website',
-      dueDate: '2024-05-31',
-    },
-    {
-      id: 't3',
-      userId: 'u3',
-      title: 'Prepare issue template',
-      summary:
-        'Prepare and describe an issue template which will help with project management',
-      dueDate: '2024-06-15',
-    },
-  ];
+  
+  constructor(private tasksService: TasksService) {}
 
   get selectedUserTasks() {
-    return this.tasks.filter((task) => task.userId === this.userId);
+    return this.tasksService.getUsersTasks(this.userId);
   }
 
   onCompleteTask(id: string) {
-    // show only the tasks that do NOT include the task with the id passed in
-    // this imitates a delete button but does not actually alter the array
-    this.tasks = this.tasks.filter((task) => task.id !== id);
+    
   }
 
   onStartAddTask() {
     this.isAddingTask = true;
-    // this.tasks.push({
-    //   id: 'f5',
-    //   userId: this.userId,
-    //   title: 'New Task',
-    //   summary: 'This is a new task',
-    //   dueDate: '2024-12-05'
-    // })
   }
 
   onCancelAddTask() {
@@ -66,13 +36,7 @@ export class TasksComponent {
   }
 
   onAddTask(taskData: NewTaskData) {
-    this.tasks.unshift({
-      id: new Date().getTime().toString(),
-      userId: this.userId,
-      title: taskData.title,
-      summary: taskData.summary,
-      dueDate: taskData.date
-    })
+
     this.isAddingTask = false;
   }
 
